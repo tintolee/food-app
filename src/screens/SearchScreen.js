0507e1dbefc01a1b710a049dee113d425e84cd05
tinto <toso4u@gmail.com>
2021-11-24 14:34:34 +0100

    created the search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View, } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+     <View>
+  <Text style={styles.text}>Hey there </Text>
+      
+
+      </View>
+  )
+ 
+  
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default SearchScreen;
